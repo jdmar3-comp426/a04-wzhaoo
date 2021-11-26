071383dccec560e7fa796ebca579313042865dc5
wzhaoo <willxiaozhao@gmail.com>
2021-11-26 13:32:18 -0500

    5

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new/", (req, res) =>{
 	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)')
 	const info = stmt.run(req.body.user, md5(req.body.pass))
-	res.status(201).json({message: `1 record created: ID ${info.lastInsertRowid} (201)`});
+	res.status(201).json({"message": info.changes + " record created: ID " + info.lastInsertRowid + " (201)"});
 })
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users/", (req, res) => {	
@@ -43,13 +43,13 @@ app.get("/app/user/:id", (req, res)=>{
 app.patch("/app/update/user/:id", (req, res)=>{
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id=?")
 	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id)
-	res.status(200).json({message: `1 record updated: ID ${req.params.id} (200)`});
+	res.status(200).json({"message": info.changes + " record updated: ID " + info.lastInsertRowid + " (200)"});
 })
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res)=>{
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id=?")
 	const info = stmt.run(req.params.id)
-	res.status(200).json({message: `1 record deleted: ID ${req.params.id} (200)`});
+	res.status(200).json({"message": info.changes + " record deleted: ID " + info.lastInsertRowid + " (200)"});
 })
 // Default response for any other request
 app.use(function(req, res){
